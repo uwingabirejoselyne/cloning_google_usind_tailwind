5297636997ad6ed2cb411bb33170e4e9ba001bb9
Joselyne Uwingabire <uwingajoselyne@gmail.com>
2023-06-14 10:00:22 +0200

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      height: {
+        '10': '2.7rem', // Custom height value
+      },
+    },
+  },
+  plugins: [],
+}
+
